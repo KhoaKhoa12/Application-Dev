952d1046a3d965df8d92d1aca4655e2dff11a196
Khoa060901 <khoanvgcd191277@fpt.edu.vn>
2023-04-13 22:41:53 +0700

    Add project files.

---
+++ Application-Dev/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Application_Dev.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
