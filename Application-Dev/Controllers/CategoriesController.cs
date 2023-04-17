32cff17976d2f778c53daf7f9eb85b7e3ca79ce6
Khoa060901 <khoanvgcd191277@fpt.edu.vn>
2023-04-18 00:05:01 +0700

    CRUD Categories

---
+++ Application-Dev/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using Application_Dev.Data;
+using Application_Dev.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application_Dev.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+
+        // 2 - Create Category Data
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            var newCategory = new Category
+            {
+                NameCategory = category.NameCategory,
+                Status = Enums.CategoryStatus.InProgess
+            };
+
+            _context.Add(newCategory);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
