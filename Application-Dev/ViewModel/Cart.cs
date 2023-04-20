2a885fc5b3958ed0d07f23c707b5b4b00d416031
quocviet2502 <vietquoctran2502@gmail.com>
2023-04-21 03:01:35 +0700

    purchase function

---
+++ Application-Dev/ViewModel/Cart.cs
@@ -0,0 +1,15 @@
+﻿using Application_Dev.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Application_Dev.ViewModel
+{
+	public class Cart
+	{
+
+		[BindProperty]
+		public List<OrderDetail> orderDetails { get; set; }
+		public int totalPrice { get; set; }
+		
+	}
+}
---
+++ Application-Dev/ViewModel/CheckOut.cs
@@ -0,0 +1,15 @@
+﻿using Application_Dev.Models;
+using System.Collections.Generic;
+
+namespace Application_Dev.ViewModel
+{
+	public class CheckOut
+	{
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string Phone { get; set; }
+		public List<OrderDetail> orderDetails { get; set; }
+		public int TotalPrice { get; set; }
+
+	}
+}
